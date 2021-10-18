6754d3bb710195d06cbfb2b2bc39a93d887c2021
MarcosHDBarbosa <50962208+MarcosHDBarbosa@users.noreply.github.com>
2021-10-18 18:02:06 -0300

    conexão heroko

---
+++ src/db/config.ts
@@ -0,0 +1,19 @@
+import { Student } from "../entities/Students"
+import { createConnection } from "typeorm"
+
+export const newConnection = async () => {
+    await createConnection ({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        entities: [Student],
+        synchronize: true,
+        extra: {
+            ssl:{
+                rejectUnauthorized: false
+            },
+        },
+    });
+
+    console.log('Conectado ao DataBase');
+};
+
